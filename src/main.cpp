dbe6d466d792a058761acf335c851756d5de3235
cmdc0de <cmdc0dez@gmail.com>
2021-12-05 00:49:33 -0700

    basics workgin

--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,128 @@
 #include <iostream>
 
 #include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 #include <docopt/docopt.h>
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
 static constexpr auto USAGE =
-  R"(Naval Fate.
+  R"(GServer3.
 
-    Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
+	Usage:
+		gserver3 start [--gui]
+		gserver3 (--help)
+		gserver3 --version
+
+
+	Options:
+		--version	Show version.
+		--gui	gui.
+		--help	HELP.
+
 )";
 
-int main(int argc, const char **argv)
-{
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
+enum MODE {
+  CMDLINE
+  , GUI
+};
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << "=" << arg.second << std::endl;
-  }
+static MODE Mode = CMDLINE;
+
+static std::shared_ptr<spdlog::logger> slog; 
+
+std::shared_ptr<spdlog::logger> &getLogger() {
+	return slog;
+}
+
+void initLoggers() {
+	spdlog::init_thread_pool(8192, 1);
+
+	auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt >();
+	stdout_sink->set_level(spdlog::level::trace);
+	
+
+	auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("slog.txt", 1024*1024*10, 3);
+	rotating_sink->set_level(spdlog::level::info);
+
+	std::vector<spdlog::sink_ptr> sinks {stdout_sink, rotating_sink};
+	slog = std::make_shared<spdlog::async_logger>("slog", sinks.begin(), sinks.end(), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
+
+	slog->set_pattern("[%H:%M:%S.%f] [%t:%n] [%^%l%$] %v");
+	spdlog::register_logger(slog);
+	spdlog::set_default_logger(slog);
+	slog->set_level(spdlog::level::trace);
+}
 
 
-  //Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Hello, {}!", "World");
+int main(int argc, const char **argv) {
 
-  fmt::print("Hello, from {}\n", "{fmt}");
+	initLoggers();
+
+	std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
+		{ std::next(argv), std::next(argv, argc) },
+		true, // show help if requested
+		"v3"  // version string
+		);
+
+	int count = 0;
+	for (auto const &arg : args) {
+		std::cout << count++ << ": " << arg.first << "=" << arg.second << std::endl;
+		if(arg.first=="--gui" && arg.second.asBool()) {
+			Mode=GUI;
+		}
+	}
+
+
+	//Use the default logger (stdout, multi-threaded, colored)
+	spdlog::info("Hello, {}!", "World");
+	getLogger()->trace("trace message: std out only");
+	getLogger()->debug("debug message std out only");
+	getLogger()->info("info message both");
+	getLogger()->flush();
+	
+
+  //fmt::print("Hello, from {}\n", "{fmt}");
+ 
+	if(GUI==Mode) { 
+		sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+		window.setFramerateLimit(60);
+		ImGui::SFML::Init(window);
+
+		sf::CircleShape shape(100.f);
+		shape.setFillColor(sf::Color::Green);
+
+		sf::Clock deltaClock;
+		while (window.isOpen()) {
+			sf::Event event;
+			while (window.pollEvent(event)) {
+				ImGui::SFML::ProcessEvent(event);
+
+				if (event.type == sf::Event::Closed) {
+					window.close();
+				}
+			}
+
+			ImGui::SFML::Update(window, deltaClock.restart());
+
+			ImGui::Begin("Hello, world!");
+			ImGui::Button("Look at this pretty button");
+			ImGui::End();
+
+			window.clear();
+			window.draw(shape);
+			ImGui::SFML::Render(window);
+			window.display();
+		}
+
+		ImGui::SFML::Shutdown();
+	}
+
+	return 0;
 }
