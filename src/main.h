17d09c0f7b1341f30c7d6ec08f6896512a1c7988
cmdc0de <cmdc0dez@gmail.com>
2021-12-24 23:12:30 -0700

    adding missing files

---
+++ src/main.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
+std::shared_ptr<spdlog::logger> &getLogger();
